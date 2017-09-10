8553770fe8f9d459c695f215855102e9c1633e26
hisazawa <d.hisazawa@gmail.com>
2017-09-11 08:39:03 +0900

    some class added

--- rbcDevApp/ContetsListViewController.swift
+++ rbcDevApp/ContetsListViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import PageMenu
 
 class ContetsListViewController: UIViewController {
 
+    var pageMenu: CAPSPageMenu?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        var controllerArray : [UIViewController] = []
+        
+        var controller : UIViewController = UIViewController(nibName: "controllerNibName", bundle: nil)
+        controller.title = "SAMPLE TITLE"
+        controllerArray.append(controller)
+        
+        
+        
+        var parameters: [CAPSPageMenuOption] = [
+            .menuItemSeparatorWidth(4.3),
+            .useMenuLikeSegmentedControl(true),
+            .menuItemSeparatorPercentageHeight(0.1)
+        ]
+        // Initialize page menu with controller array, frame, and optional parameters
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x:0.0,y: 0.0, width:self.view.frame.width, height:self.view.frame.height), pageMenuOptions: parameters)
+        
+        // Lastly add page menu as subview of base view controller view
+        // or use pageMenu controller in you view hierachy as desired
+        self.view.addSubview(pageMenu!.view)
+        // Optional delegate
+        pageMenu!.delegate = self as? CAPSPageMenuDelegate
+        
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
